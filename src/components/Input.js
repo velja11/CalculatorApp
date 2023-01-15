8a586f0e2b85984b4f8b1fdf81d8643a6c247c75
Velja <velibor1418@gmail.com>
2023-01-15 23:27:30 +0100

    fixing input type

--- src/components/Input.js
+++ src/components/Input.js
@@ -17,7 +17,7 @@ const Input = ({
                 styleSelectField="p-2 bg-gray-200 text-center shadow-sm hover:cursor-pointer rounded-r-sm"
             />
             <input
-                type="text"
+                type="number"
                 id="income"
                 className="border-2 border-gray-200 mt-4 outline-none w-full h-10 rounded-md pl-6 focus:border-2 focus:border-green-300"
                 min="0"
